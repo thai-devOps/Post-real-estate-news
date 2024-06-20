5b7160a1630da93996aea5eea23db8ee7952bf52
Chau Thai <thai.dev.2601@gmail.com>
2024-06-20 13:23:37 +0700

    update payment

--- bds_server/src/services/payments.service.ts
+++ bds_server/src/services/payments.service.ts
@@ -16,7 +16,43 @@ class PaymentService {
     )
   }
   async getAllPayments() {
-    return await databaseService.payments.find().toArray()
+    return await databaseService.payments
+      .aggregate([
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'user_id',
+            foreignField: '_id',
+            as: 'user'
+          }
+        },
+        {
+          $lookup: {
+            from: 'vip_packages',
+            localField: 'package_id',
+            foreignField: '_id',
+            as: 'package'
+          }
+        },
+        {
+          $project: {
+            _id: 1,
+            payment_date: 1,
+            payment_method: 1,
+            is_paid: 1,
+            status: 1,
+            amount: 1,
+            currency: 1,
+            user: {
+              $arrayElemAt: ['$user', 0]
+            },
+            package: {
+              $arrayElemAt: ['$package', 0]
+            }
+          }
+        }
+      ])
+      .toArray()
   }
   async getPaymentById(id: string) {
     return await databaseService.payments.findOne({ _id: new ObjectId(id) })
